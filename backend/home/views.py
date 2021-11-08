d4932dfadc0d7dd569e310601bded377dd609482
Demetre <demetre.jouras@gmail.com>
2021-11-07 19:55:07 -0500

    dockerize app

--- backend/home/views.py
+++ backend/home/views.py
@@ -11,8 +11,3 @@ def get_time(request):
     cursor.execute('''select date_trunc('minute', now())''')
     current_datetime = cursor.fetchone()[0]
     return HttpResponse(f"{current_datetime.strftime('%Y-%m-%d %H:%M:%S')}")
-
-
-def trigger_sentry(request):
-    division_by_zero = 1 / 0
-    return HttpResponse(division_by_zero)
