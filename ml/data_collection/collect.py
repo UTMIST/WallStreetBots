b2f905384667383055da26c73f3fef769795359d
Demetre <demetre.jouras@gmail.com>
2021-11-19 13:27:34 -0500

    improve rss collection

--- ml/data_collection/collect.py
+++ ml/data_collection/collect.py
@@ -1,23 +1,43 @@
 import queue
-import threading
-import time
 import feedparser
+from typing import Optional, Dict
 
-q = queue.Queue()
+q = queue.Queue() # queue of processed items ready to be ingested by data pipeline
+rss_sources = []  # list of rss sources with their own processing function
 
 
-def wait_for_newest_rss_feed_entry(url):
-    feed = feedparser.parse(url)
-    newest_entry_id = None
+def process_rss_feed_entry(entry: feedparser.util.FeedParserDict) -> Dict[str, str]:
+    return {"title": entry.title, "summary": entry.summary}
+
+
+class RssSource:
+    url: str
+    process_entry: callable
+    _newest_id: Optional[str] = None
+
+    def __init__(self, url, process_entry):
+        self.url = url
+        self.process_entry = process_entry
+        self._newest_id = None
+
+    def get_newest_entry(self):
+        feed = feedparser.parse(self.url)
+        entry = feed['entries'][0]
+        if self._newest_id is None or entry['id'] != self._newest_id:
+            self._newest_id = entry['id']
+            return feed.entries[0]
+        return None
+
+
+def wait_for_newest_rss_feed_entry():
+    i = 0
     while True:
-        entries = feed['entries']
-        if len(entries) > 0 and newest_entry_id != entries[0]['id']:
-            newest_entry_id = entries[0]['id']
-            print(newest_entry_id)
-            q.put(newest_entry_id)
-        else:
-            time.sleep(5)
-            print("waiting")
+        newest_rss_item = rss_sources[i].get_newest_entry()
+        if newest_rss_item:
+            q.put(newest_rss_item)
+
+        i = (i + 1) % len(rss_sources)
+
 
 
 urls = [
@@ -28,9 +48,5 @@ urls = [
 
 if __name__ == "__main__":
     for url in urls:
-        t = threading.Thread(target=wait_for_newest_rss_feed_entry, args=(url,))
-        t.start()
-    while True:
-        if not q.empty():
-            print("getting from queue")
-            print(q.get())
+        rss_sources.append(RssSource(url, lambda x: x))
+    wait_for_newest_rss_feed_entry()
