f737f8c87b418281f907b9d10b36c401e031f770
Demetre <demetre.jouras@gmail.com>
2022-03-04 17:09:25 -0500

    trigger GH actions

--- backend/tradingbot/apps.py
+++ backend/tradingbot/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class TradingbotConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'backend.tradingbot'
+    verbose_name = 'Trading Bot'
