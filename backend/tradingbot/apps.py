c341670c0cda72a92f874c10700fbbc64b62a1d5
Demetre <demetre.jouras@gmail.com>
2022-03-12 12:18:21 -0500

    add period scheduler

--- backend/tradingbot/apps.py
+++ backend/tradingbot/apps.py
@@ -1,28 +1,35 @@
 from django.apps import AppConfig
+from datetime import datetime
+from apscheduler.schedulers.background import BackgroundScheduler
+
+
+def create_portfolio_dictionary(portfolio):
+    from .models import StockInstance
+    result = {'cash': float(portfolio.cash)}
+    stock_instances = StockInstance.objects.filter(portfolio=portfolio)
+    result['stocks'] = []
+    for stock_instances in stock_instances:
+        result['stocks'].append(stock_instances.stock.company.ticker)
+    return result
+
+
+def start_pipelines():
+    from django.contrib.auth.models import User
+    from ml.tradingbots.pipelines.monte_carlo_w_ma import MonteCarloMovingAveragePipline
+    pipelines = []
+    for user in User.objects.all():
+        if user.portfolio:
+            pipelines.append(MonteCarloMovingAveragePipline("MonteCarlo", create_portfolio_dictionary(user.portfolio)))
+
+    for pipeline in pipelines:
+        pipeline.rebalance()
 
 
 class TradingbotConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'backend.tradingbot'
 
-    def create_portfolio_dictionary(self, portfolio):
-        # import django
-        # django.setup()
-        from .models import StockInstance
-        result = {'cash': float(portfolio.cash)}
-        stock_instances = StockInstance.objects.filter(portfolio=portfolio)
-        result['stocks'] = []
-        for stock_instances in stock_instances:
-            result['stocks'].append(stock_instances.stock.company.ticker)
-        return result
-
     def ready(self):
-        from django.contrib.auth.models import User
-        from ml.tradingbots.pipelines.monte_carlo_w_ma import MonteCarloMovingAveragePipline
-        pipelines = []
-        for user in User.objects.all():
-            if user.portfolio:
-                pipelines.append(MonteCarloMovingAveragePipline("MonteCarlo", self.create_portfolio_dictionary(user.portfolio)))
-
-        for pipeline in pipelines:
-            pipeline.rebalance()
+        scheduler = BackgroundScheduler()
+        scheduler.add_job(start_pipelines, 'interval', seconds=5)
+        scheduler.start()
