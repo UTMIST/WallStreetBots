05a58f8b6c0b7b4e212875571f54a32e6f4d12b3
Demetre <demetre.jouras@gmail.com>
2022-03-23 13:57:37 -0400

    dynamically pick rebalancing strategy

--- backend/tradingbot/apps.py
+++ backend/tradingbot/apps.py
@@ -15,10 +15,12 @@ def create_portfolio_dictionary(portfolio):
 def start_pipelines():
     from django.contrib.auth.models import User
     from ml.tradingbots.pipelines.monte_carlo_w_ma import MonteCarloMovingAveragePipline
+    rebalancing_strategies = {"monte_carlo": MonteCarloMovingAveragePipline}
     pipelines = []
     for user in User.objects.all():
         if user.portfolio:
-            pipelines.append(MonteCarloMovingAveragePipline("MonteCarlo", create_portfolio_dictionary(user.portfolio)))
+            rebalancing_strategy = rebalancing_strategies[user.portfolio.rebalancing_strategy]
+            pipelines.append(rebalancing_strategy("MonteCarlo", create_portfolio_dictionary(user.portfolio)))
 
     for pipeline in pipelines:
         pipeline.rebalance()
@@ -30,5 +32,7 @@ class TradingbotConfig(AppConfig):
 
     def ready(self):
         scheduler = BackgroundScheduler()
+        # TODO: change this interval from 5 seconds to something more reasonable
+        # currently 5 seconds to make it easy to check that it is working
         scheduler.add_job(start_pipelines, 'interval', seconds=5)
         scheduler.start()
